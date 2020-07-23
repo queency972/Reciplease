91ecea2837442e3686b07d41c5edca8c53f277bd
queency972 <steve.bernard972@gmail.com>
2020-07-23 13:21:17 -0500

    Setup IBOutlet "Summary"

--- Reciplease/Controller/Search/RecipeDetailViewController.swift
+++ Reciplease/Controller/Search/RecipeDetailViewController.swift
@@ -11,46 +11,38 @@ import UIKit
 //
 class RecipeDetailViewController: UIViewController {
 
-
-
-
-
-
-    var recipe: Recipe!
+    @IBOutlet weak var recipeTitleLabel: UILabel!
+    @IBOutlet weak var recipeImage: UIImageView!
+    @IBOutlet weak var preparationTimeLabel: UILabel!
     @IBOutlet weak var tableView: UITableView!
 
-        override func viewDidLoad() {
-            super.viewDidLoad()
-            print(recipe)
-        }
+    var recipe: Recipe?
 
-    var recipes: Recipe? {
-        didSet {
-            //titleLabel.text = recipe?.label
-            //detailLabel.text = "\(String(describing: ingredient.allIngredients))"
-           //preparationTimeLabel.text = "\(String(describing: recipe!.totalTime)) mins"
-            //recipeImage.sd_setImage(with: URL(string: "\(recipe?.image ?? "")"), placeholderImage: UIImage(named: "Cooking.png"))
-        }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        recipeTitleLabel.text = recipe?.label
+        preparationTimeLabel.text = "\(String(describing: recipe!.totalTime)) min(s)"
+        recipeImage.sd_setImage(with: URL(string: "\(recipe?.image ?? "")"), placeholderImage: UIImage(named: "Cooking.png"))
     }
 }
 
-    // MARK: - TableView
-    extension RecipeDetailViewController: UITableViewDataSource, UITableViewDelegate {
+// MARK: - TableView
+extension RecipeDetailViewController: UITableViewDataSource, UITableViewDelegate {
 
-        // numberof line need
-        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-            recipe.ingredientLines.count
-        }
-
-        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-            let cell = tableView.dequeueReusableCell(withIdentifier: "ingredients", for: indexPath)
-            cell.textLabel?.text = recipe.ingredientLines[indexPath.row]
-            cell.textLabel?.font = UIFont(name:"Noteworthy", size:18)
-            return cell
-        }
-
-        // Use heightForFooterInSection (line) if necessary
-        func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
-            return recipe.ingredientLines.isEmpty ? 50 : 1
-        }
+    // numberof line need
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        (recipe?.ingredientLines.count)!
     }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ingredients", for: indexPath)
+        cell.textLabel?.text = recipe?.ingredientLines[indexPath.row]
+        cell.textLabel?.font = UIFont(name:"Noteworthy", size:18)
+        return cell
+    }
+
+    // Use heightForFooterInSection (line) if necessary
+    func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        return (recipe?.ingredientLines.isEmpty)! ? 50 : 1
+    }
+}
